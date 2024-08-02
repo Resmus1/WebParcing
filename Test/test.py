bb892de001b794f87e92df0a9324a4158a713be6
Resmus <elbandito237@gmail.com>
2024-08-02 17:37:26 +0600

    Move in Project

--- Test/test.py
+++ Test/test.py
@@ -1,7 +1,23 @@
 import time
+from selenium.webdriver import Keys
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+with webdriver.Chrome() as browser:
+    browser.get(r"https://parsinger.ru/selenium/5.7/3/index.html")
 
-with webdriver.Chrome() as driver:
-    driver.get("https://stepik.org/course/104774")
-    time.sleep(5)
+    list_input = []      # Инициализируем пустой список для хранения обработанных элементов ввода
+    while True:          # Начинаем бесконечный цикл
+
+        # Ищем все элементы input на веб-странице и добавляем их в список input_tags
+        input_tags = [x for x in browser.find_elements(By.TAG_NAME, 'input')]
+
+        # Обходим каждый элемент input в списке
+        for tag_input in input_tags:
+            # Проверяем, не обрабатывали ли мы уже этот элемент ранее
+            if tag_input not in list_input:
+                tag_input.send_keys(Keys.DOWN)     # Отправляем клавишу "Вниз"
+                browser.execute_script("return arguments[0].scrollIntoView(true);", tag_input)
+                tag_input.click()                  # Кликаем на элемент
+                time.sleep(.3)
+                list_input.append(tag_input)       # Добавляем элемент в список обработанных элементов
