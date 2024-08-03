f84adf53c72dd02c014a63a766387a14c8b4f7ed
Resmus <elbandito237@gmail.com>
2024-08-03 10:03:17 +0600

    upd

---
+++ Requests/1.py
@@ -0,0 +1,9 @@
+# Копирование картинок из сайта перебором через url
+import requests
+from fake_useragent import UserAgent
+
+headers = {'User-Agent': UserAgent().random}
+for i in range(1, 161):
+    response = requests.get(url=f'https://parsinger.ru/img_download/img/ready/{i}.png', headers=headers)
+    with open(f'image{i}.png', 'wb') as image:
+        image.write(response.content)
---
+++ Requests/2.py
@@ -0,0 +1,10 @@
+# Извлекаем информацию из скаченной страницы
+
+from bs4 import BeautifulSoup
+
+
+file_path = r'C:\PyCharm\WebParcing\Requests\files\index.html'
+with open(file_path, 'r', encoding='utf-8') as file:
+    content = file.read()
+soup = BeautifulSoup(content, 'lxml')
+print(soup)
