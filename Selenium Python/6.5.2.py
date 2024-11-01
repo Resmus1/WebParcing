21c337b199d17acff6e7bb4e14bdc21af7ca336f
ReSmus <elbandito234@gmail.com>
2024-11-01 12:50:54 +0600

    UPD Изменил решение подогнал под условия

--- Selenium Python/6.5.2.py
+++ Selenium Python/6.5.2.py
@@ -1,6 +1,4 @@
 # https://stepik.org/lesson/732065/step/1?unit=733598
-# Здесь нужно использовать именно ActionChains а не упрощать работу с помощью java
-# Сделал прерывание списка по своему, а не по последним элементу
 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -17,31 +15,33 @@ with webdriver.Chrome(options=options_chrome) as browser:
     # Переходим на целевую страницу
     browser.get('https://parsinger.ru/infiniti_scroll_1/')
 
-    unique_numbers = set()  # Используем множество для хранения уникальных чисел
     sum_num = 0
+    unique_numbers = set()  # Для хранения уникальных чисел
 
-    scroll_element = browser.find_element(By.ID, 'scroll-wrapper')
+    scroll_element = browser.find_element(By.ID, 'scroll-container')
 
     while True:
-        action = ActionChains(browser)
-        action.click(scroll_element).key_down(Keys.PAGE_DOWN).perform()
+        actions = ActionChains(browser)
+        actions.click(scroll_element).key_down(Keys.PAGE_DOWN).perform()
 
-        # Ждем, чтобы дать странице время на подгрузку новых элементов
+        # Ждем, чтобы дать странице время на подгруздку новых элементов
         time.sleep(1)
 
         nums_elements = scroll_element.find_elements(By.TAG_NAME, 'span')
 
-        new_elements = False
-
         for i in nums_elements:
             num = i.text.strip()
-            if num.isdigit() and num not in unique_numbers:  # Проверка на уникальность и числовое значение
-                unique_numbers.add(num)  # Добавляем новое число в множество
-                sum_num += int(num)  # Сразу добавляем к общей сумме
-                new_elements = True
+            if num.isdigit() and num not in unique_numbers:  # Проверка на уникальность
+                sum_num += int(num)
+                unique_numbers.add(num)  # Добавление уникального числа в множество
 
-        # Если новые элементы не были добавлены, выходим из цикла
-        if not new_elements:
-            print("Больше нет новых элементов для добавления.")
-            print("Сумма числовых значений:", sum_num)
-            break
+        # Проверяем, достигнут ли элемент с классом last-of-list
+        try:
+            last_of_list_element = scroll_element.find_element(By.CLASS_NAME, 'last-of-list')
+            if last_of_list_element.is_displayed():  # Если элемент виден, выходим из цикла
+                print("Достигнут элемент 'last-of-list'. Завершение сбора данных.")
+                break
+        except Exception:
+            pass  # Если элемент не найден, продолжаем
+
+    print("Сумма числовых значений:", sum_num)
